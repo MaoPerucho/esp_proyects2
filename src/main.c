1fa6c76c2ba24f8141537ad6c9b8ae292b6e6349
Hedinyer Perucho <hedinyer.perucho@ustabuca.edu.co>
2021-02-22 07:46:52 -0500

    codigo Delay2

--- src/main.c
+++ src/main.c
@@ -1,1 +1,17 @@
-void app_main() {}
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
+
+#define TAG "DELAY"
+
+void app_main() 
+{
+    int i = 0;
+    while (1)
+    {
+        vTaskDelay(1000/portTICK_PERIOD_MS);
+        ESP_LOGI(TAG,"Loop %d: ",i++);
+    }
+    
+}
